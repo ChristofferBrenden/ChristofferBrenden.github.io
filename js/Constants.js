0dd22f00172ef344185de2fab150c84fc3359753
Christoffer Brenden <christofferbrenden@hotmail.com>
2022-06-30 11:48:27 +0200

    Link to /Snake

--- js/Constants.js
+++
@@ -1,5 +0,0 @@
-const Constants = {
-    FPS: 8,
-    SPRITE_IMAGE: './snake-graphics.png',
-    SPRITE_PIXEL_SIZE: 64
-}
--- js/Main.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    var game = new Game();
-
-    window.addEventListener("keydown", (event) => {
-        game.handleInput(event);
-    });
-};
